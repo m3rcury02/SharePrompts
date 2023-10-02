9f946f5e80e79700234b3c991efe60c4571a7a5a
Gunal Seenivasagan <gunal2002@gmail.com>
2023-10-03 02:11:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
